0fb32e1d968d03c8577cc9012dd1db092dbc1292
RinHizakura <s921975628@gmail.com>
2022-10-13 20:42:40 +0800

    Add helper to transform Parker into Waker

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,43 @@ mod parking;
 
 use crate::parking::Parker;
 use std::future::Future;
+use std::sync::Arc;
+use std::task::{RawWaker, RawWakerVTable, Waker};
 
+// create a Waker from our Parker
+fn create_waker(parker: Parker) -> Waker {
+    let raw = Arc::into_raw(Arc::new(parker)) as *const ();
+    let vtable = &ParkerVtable::VTABLE;
+    unsafe { Waker::from_raw(RawWaker::new(raw, vtable)) }
+}
+
+// the VTABLE struct is used to transform our Parker as Waker
+struct ParkerVtable;
+impl ParkerVtable {
+    const VTABLE: RawWakerVTable =
+        RawWakerVTable::new(Self::clone, Self::wake, Self::wake_by_ref, Self::drop);
+
+    unsafe fn clone(ptr: *const ()) -> RawWaker {
+        todo!();
+    }
+
+    unsafe fn wake(ptr: *const ()) {
+        todo!();
+    }
+
+    unsafe fn wake_by_ref(ptr: *const ()) {
+        todo!();
+    }
+
+    unsafe fn drop(ptr: *const ()) {
+        todo!();
+    }
+}
+
+// executor
 pub fn block_on<T: Default>(future: impl Future<Output = T>) -> T {
     let parker = Parker::new();
+    let waker = create_waker(parker);
 
     // TODO: just for success compiling
     T::default()
